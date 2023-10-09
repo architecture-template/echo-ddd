b9631acede44d4b7ad0c5ccd04e06d9fe806fc0d
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-10-09 16:29:41 +0900

    Daoテストを追加

--- infra/dao/user_test.go
+++ infra/dao/user_test.go
@@ -75,3 +75,72 @@ func TestUser_FindByEmail(t *testing.T) {
 		})
 	}
 }
+
+func TestUser_Insert(t *testing.T) {
+    testCases := []struct {
+        name             string
+        mockParam        *model.User
+        mockRowsAffected int64
+        mockLastInsertID int64
+        mockError        error
+        expectedUser     *model.User
+        expectedError    error
+    }{
+        {
+            name: "Successful",
+            mockParam: &model.User{
+                UserKey:  "test_key",
+                UserName: "test_name",
+                Email:    "test@test.com",
+                Password: "test_password",
+                Token:    "test_token",
+            },
+            mockRowsAffected: 1,
+            mockLastInsertID: 1,
+            mockError:        nil,
+            expectedUser: &model.User{
+                ID:        1,
+                UserKey:   "test_key",
+                UserName: "test_name",
+                Email:     "test@test.com",
+                Password:  "test_password",
+                Token:     "test_token",
+				CreatedAt: time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+				UpdatedAt: time.Date(1, time.January, 1, 0, 0, 0, 0, time.UTC),
+            },
+            expectedError: nil,
+        },
+    }
+
+    for _, tc := range testCases {
+        t.Run(tc.name, func(t *testing.T) {
+            db, mock, _ := sqlmock.New()
+            defer db.Close()
+
+			gormDB, _ := gorm.Open("mysql", db)
+			sqlHandler := &dbConfig.SqlHandler{
+				ReadConn:  gormDB,
+				WriteConn: gormDB,
+			}
+
+			repo := NewUserDao(sqlHandler)
+            mock.ExpectBegin()
+            mock.ExpectExec("INSERT").
+                WillReturnResult(sqlmock.NewResult(tc.mockLastInsertID, tc.mockRowsAffected)).
+                WillReturnError(tc.mockError)
+            mock.ExpectCommit()
+
+            user, err := repo.Insert(tc.mockParam, gormDB)
+            if tc.expectedError != nil {
+                assert.EqualError(t, err, tc.expectedError.Error())
+            } else {
+                assert.NoError(t, err)
+            }
+
+            user.CreatedAt = time.Time{}
+            user.UpdatedAt = time.Time{}
+
+            assert.Equal(t, tc.expectedUser, user)
+        })
+    }
+}
